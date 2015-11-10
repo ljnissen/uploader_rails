89219845c052fd96cbc45063855a8ff53a828198
LJ Nissen <lj.nissen@gmail.com>
2015-11-10 21:34:01 +0100

    Updated gems

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'access/index'
+
+  get 'access/show'
+
+  get 'access/new'
+
+  get 'access/edit'
+
+  get 'access/delete'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
